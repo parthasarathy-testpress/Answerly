8c6d341e770820625f8b7af5d1463f16441a17bb
parthasarathy AK <parthasarathy@testpress.in>
2025-10-14 09:41:15 +0000

    chore: Initial commit

---
+++ Answerly/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Answerly project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Answerly.settings')
+
+application = get_asgi_application()
