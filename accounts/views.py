71cb369da69d2d7280f36fe2100589c503be520a
Parthasarathy AK <parthasarathy@testpress.in>
2025-10-16 17:46:53 +0530

    fix: Render HTML in password reset emails (#5)
    - Password reset emails were sending HTML content as plain text, making
    links and formatting unusable.
    - This happened because the default view does not render email templates
    as HTML.
    - Fixed by using a custom view that correctly renders the password reset
    template with HTML content.

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,8 @@ from django.views.generic.edit import FormView
 from django.contrib.auth import login
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.views import PasswordResetView
+from django.contrib.messages.views import SuccessMessageMixin
 
 class SignupView(FormView):
     template_name = 'accounts/signup.html'
@@ -14,3 +16,8 @@ class SignupView(FormView):
         login(self.request, user)
         messages.success(self.request, f'Account created for {user.username}!')
         return super().form_valid(form)
+
+class CustomPasswordResetView(SuccessMessageMixin, PasswordResetView):
+    html_email_template_name = 'registration/password_reset_email.html'
+    success_url = reverse_lazy('password_reset_done')
+    success_message = "Password reset email sent!"
