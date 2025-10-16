f1b860ad9b518f0ef14680d34a702d0536ff039a
parthasarathy-testpress <parthasarathy@testpress.in>
2025-10-16 14:55:23 +0530

    fix: Render HTML in password reset emails
    - Password reset emails were sending HTML content as plain text, making links and formatting unusable.
    - This happened because the default view does not render email templates as HTML.
    - Fixed by using a custom view that correctly renders the password reset template with HTML content.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import SignupView
+from .views import SignupView,CustomPasswordResetView
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
+    path('password_reset/', CustomPasswordResetView.as_view(), name='password_reset'),
     path('', include('django.contrib.auth.urls')),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,10 @@ from django.views.generic.edit import FormView
 from django.contrib.auth import login
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.forms import PasswordResetForm
+from django.views import View
+from django.shortcuts import render, redirect
+from django.conf import settings
 
 class SignupView(FormView):
     template_name = 'accounts/signup.html'
@@ -14,3 +18,24 @@ class SignupView(FormView):
         login(self.request, user)
         messages.success(self.request, f'Account created for {user.username}!')
         return super().form_valid(form)
+
+class CustomPasswordResetView(View):
+    template_name = 'registration/password_reset_form.html'
+
+    def get(self, request):
+        form = PasswordResetForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = PasswordResetForm(request.POST)
+        if form.is_valid():
+            form.save(
+                request=request,
+                use_https=request.is_secure(),
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                email_template_name='registration/password_reset_email.txt',
+                html_email_template_name='registration/password_reset_email.html',
+            )
+            messages.success(request, "Password reset email sent!")
+            return redirect(reverse_lazy('password_reset_done'))
+        return render(request, self.template_name, {'form': form})
