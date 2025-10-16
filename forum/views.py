2d83fefdd94da94f67e94943313d631a2da3febf
parthasarathy-testpress <parthasarathy@testpress.in>
2025-10-16 18:13:16 +0530

    feat: Add home page with question list and pagination
    - Implemented the home page to display a list of questions.
    - Added pagination to limit the number of questions displayed per page.
    - Ensures users can navigate through multiple pages of questions easily.

--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.db.models import Count, Q
+from .models import Question, Vote
 
-# Create your views here.
+class QuestionListView(ListView):
+    model = Question
+    template_name = 'questions/question_list.html'
+    context_object_name = 'questions'
+    ordering = ['-created_at']
+    paginate_by = 10
+
+    def get_queryset(self):
+        return Question.objects.annotate(
+            total_votes=Count('votes', filter=Q(votes__vote_type=1)) - Count('votes', filter=Q(votes__vote_type=-1))
+        ).order_by('-created_at')
