aa791a5c20076937d510f037723a9d5656ca8c8f
parthasarathy-testpress <parthasarathy@testpress.in>
2025-10-16 15:04:36 +0530

    refactor: Change view with using mixin

--- accounts/views.py
+++ accounts/views.py
@@ -3,10 +3,8 @@ from django.views.generic.edit import FormView
 from django.contrib.auth import login
 from django.contrib import messages
 from .forms import UserRegisterForm
-from django.contrib.auth.forms import PasswordResetForm
-from django.views import View
-from django.shortcuts import render, redirect
-from django.conf import settings
+from django.contrib.auth.views import PasswordResetView
+from django.contrib.messages.views import SuccessMessageMixin
 
 class SignupView(FormView):
     template_name = 'accounts/signup.html'
@@ -19,23 +17,9 @@ class SignupView(FormView):
         messages.success(self.request, f'Account created for {user.username}!')
         return super().form_valid(form)
 
-class CustomPasswordResetView(View):
+class CustomPasswordResetView(SuccessMessageMixin, PasswordResetView):
     template_name = 'registration/password_reset_form.html'
-
-    def get(self, request):
-        form = PasswordResetForm()
-        return render(request, self.template_name, {'form': form})
-
-    def post(self, request):
-        form = PasswordResetForm(request.POST)
-        if form.is_valid():
-            form.save(
-                request=request,
-                use_https=request.is_secure(),
-                from_email=settings.DEFAULT_FROM_EMAIL,
-                email_template_name='registration/password_reset_email.txt',
-                html_email_template_name='registration/password_reset_email.html',
-            )
-            messages.success(request, "Password reset email sent!")
-            return redirect(reverse_lazy('password_reset_done'))
-        return render(request, self.template_name, {'form': form})
+    email_template_name = 'registration/password_reset_email.txt'
+    html_email_template_name = 'registration/password_reset_email.html'
+    success_url = reverse_lazy('password_reset_done')
+    success_message = "Password reset email sent!"
