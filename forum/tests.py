e8883eb504d954f578b2012b18821edf62f9083f
Parthasarathy AK <parthasarathy@testpress.in>
2025-10-22 11:25:41 +0530

    feat: Add home page with question list and pagination (#6)
    - Implemented the home page to display a list of questions.
    - Added pagination to limit the number of questions displayed per page.
    - Ensures users can navigate through multiple pages of questions easily.

--- forum/tests.py
+++ forum/tests.py
@@ -1,3 +1,44 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from forum.models import Question
 
-# Create your tests here.
+class QuestionListViewTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='pass123')
+        self.user2 = User.objects.create_user(username='otheruser', password='pass123')
+
+        for i in range(15):
+            Question.objects.create(
+                title=f'Question {i}',
+                description='Test description',
+                author=self.user
+            )
+
+        question1 = Question.objects.latest('created_at')
+        question1.votes.create(user=self.user, vote_type=1)
+        question1.votes.create(user=self.user2, vote_type=-1)
+
+    def test_question_list_view_status_and_template(self):
+        """Check that the list view returns 200 and uses correct template"""
+        response = self.client.get(reverse('question_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'forum/question_list.html')
+
+    def test_question_list_context(self):
+        """Check that the context contains questions and total_votes is annotated"""
+        response = self.client.get(reverse('question_list'))
+        self.assertIn('questions', response.context)
+        first_question = response.context['questions'][0]
+        self.assertTrue(hasattr(first_question, 'total_votes'))
+        # total_votes = 1 upvote - 1 downvote = 0
+        self.assertEqual(first_question.total_votes, 0)
+
+    def test_pagination(self):
+        """Check that pagination works (paginate_by = 10)"""
+        response = self.client.get(reverse('question_list'))
+        self.assertEqual(len(response.context['questions']), 10)
+
+        response_page2 = self.client.get(reverse('question_list') + '?page=2')
+        self.assertEqual(len(response_page2.context['questions']), 5)
--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.db.models import Sum
+from .models import Question, Vote
 
-# Create your views here.
+class QuestionListView(ListView):
+    model = Question
+    template_name = 'forum/question_list.html'
+    context_object_name = 'questions'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return Question.objects.annotate(
+            total_votes=Sum('votes__vote_type', default=0)
+        ).order_by('-created_at')
