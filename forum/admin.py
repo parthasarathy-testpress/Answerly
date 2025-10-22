1bf5726d8667c338e9df911a31416e88d6d0e34c
Parthasarathy AK <parthasarathy@testpress.in>
2025-10-22 12:50:31 +0530

    feat: Add admin interface for forum models (#7)
    - Registered Question, Answer, Comment, and Vote models in Django admin.
    - Added inlines for related objects: Answers and Votes under Questions;
    Comments and Votes under Answers; nested Comments and Votes under
    Comments.
    - Configured list_display and readonly_fields for better admin usability
    and data integrity.

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline
+from .models import Question, Answer, Comment, Vote
 
-# Register your models here.
+class VoteInline(GenericTabularInline):
+    model = Vote
+    extra = 0
+    readonly_fields = ("user", "vote_type", "created_at")
+
+class CommentInline(GenericTabularInline):
+    model = Comment
+    extra = 0
+    fields = ("author", "content", "parent", "created_at")
+    readonly_fields = ("created_at",)
+
+class AnswerInline(admin.TabularInline):
+    model = Answer
+    extra = 0
+    fields = ("author", "content", "created_at")
+    readonly_fields = ("created_at",)
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "created_at")
+    inlines = [AnswerInline, VoteInline]
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ("question", "author", "created_at")
+    inlines = [CommentInline, VoteInline]
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("content_object", "author", "parent", "created_at")
+    inlines = [CommentInline,VoteInline]
+
+@admin.register(Vote)
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ("content_object", "user", "vote_type", "created_at")
