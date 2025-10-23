030e3b455532c126c2da6986dc172b79d5173238
Parthasarathy AK <parthasarathy@testpress.in>
2025-10-23 10:45:02 +0530

    feat: Create question delete feature (#10)
    - Added functionality and page for deleting questions.
    - Ensures only authenticated users can access the delete action.
    - Restricts deletion so that only the original author of a question can
    remove it.

--- forum/urls.py
+++ forum/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import QuestionListView,QuestionCreateView,QuestionUpdateView
+from .views import QuestionListView,QuestionCreateView,QuestionUpdateView,QuestionDeleteView
 
 urlpatterns = [
     path('', QuestionListView.as_view(), name='question_list'),
     path('post/', QuestionCreateView.as_view(), name='question_post'),
     path('question/<int:question_id>/edit/', QuestionUpdateView.as_view(), name='question_edit'),
+    path('question/<int:question_id>/delete/', QuestionDeleteView.as_view(), name='question_delete'),
 ]
