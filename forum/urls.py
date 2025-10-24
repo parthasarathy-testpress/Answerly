e309a8bd05054fe4882f2e2ac794348c37054dac
parthasarathy-testpress <parthasarathy@testpress.in>
2025-10-24 15:27:20 +0530

    feat: Add answer delete functionality
    - Allows users to delete their own answers.
    - Ensures only the author of an answer can perform the delete action.
    - Improves content management by letting users remove outdated or incorrect answers.

--- forum/urls.py
+++ forum/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import QuestionListView,QuestionCreateView,QuestionUpdateView,QuestionDeleteView,QuestionDetailView,AnswerCreateView,AnswerUpdateView
+from .views import QuestionListView,QuestionCreateView,\
+    QuestionUpdateView,QuestionDeleteView,QuestionDetailView,\
+    AnswerCreateView,AnswerUpdateView,AnswerDeleteView
 
 urlpatterns = [
     path('', QuestionListView.as_view(), name='question_list'),
@@ -9,4 +11,5 @@ urlpatterns = [
     path('question/<int:question_id>/answer/', AnswerCreateView.as_view(), name='answer_post'),
     path('question/<int:question_id>/', QuestionDetailView.as_view(), name='question_detail'),
     path('answer/<int:answer_id>/edit/', AnswerUpdateView.as_view(), name='answer_update'),
+    path('answer/<int:answer_id>/delete/', AnswerDeleteView.as_view(), name='answer_delete'),
 ]
