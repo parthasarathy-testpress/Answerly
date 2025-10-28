152ff60b8db76e96281d86fdc273be13f9737f23
parthasarathy-testpress <parthasarathy@testpress.in>
2025-10-28 11:34:18 +0530

    feat: Add comment delete feature in answer detail page
    - Added a delete option for comments in the answer detail page.
    - Allows users to remove their own comments with a confirmation prompt, improving content management and user control.

--- forum/views.py
+++ forum/views.py
@@ -249,3 +249,23 @@ class CommentUpdateView(LoginRequiredMixin, AuthorRequiredMixin, UpdateView):
         context = super().get_context_data(**kwargs)
         context['cancel_url'] = self.get_success_url()
         return context
+
+class CommentDeleteView(LoginRequiredMixin, AuthorRequiredMixin, DeleteView):
+    model = Comment
+    template_name = "forum/comment_confirm_delete.html"
+    pk_url_kwarg = 'comment_id'
+    _success_url = None
+
+    def get_success_url(self):
+        if self._success_url is None:
+            root = self.object
+            while root.parent is not None:
+                root = root.parent
+            answer = root.content_object
+            self._success_url = reverse_lazy('answer_detail', kwargs={'answer_id': answer.pk})
+        return self._success_url
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cancel_url'] = self.get_success_url()
+        return context
