9ef5b023c361fcbdf610f751fa3b52706e1d3f2a
parthasarathy-testpress <parthasarathy@testpress.in>
2025-10-28 18:34:42 +0530

    feat: Add user profile edit page
    - Added a profile edit page allowing users to update their personal information.
    - Provides an intuitive form with validation for editing details like name,email.
    - Enhances user experience by enabling easy profile customization.

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,12 @@
 from django.urls import reverse_lazy
-from django.views.generic.edit import FormView
+from django.views.generic.edit import FormView,UpdateView
 from django.contrib.auth import login
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm,UserEditForm
 from django.contrib.auth.views import PasswordResetView
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class SignupView(FormView):
     template_name = 'accounts/signup.html'
@@ -21,3 +23,13 @@ class CustomPasswordResetView(SuccessMessageMixin, PasswordResetView):
     html_email_template_name = 'registration/password_reset_email.html'
     success_url = reverse_lazy('password_reset_done')
     success_message = "Password reset email sent!"
+
+class ProfileView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+    model = User
+    form_class = UserEditForm
+    template_name = 'accounts/profile.html'
+    success_url = reverse_lazy('question_list')
+    success_message = "Your profile was updated successfully!"
+
+    def get_object(self, queryset=None):
+        return self.request.user
