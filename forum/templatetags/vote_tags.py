2e7032cec30bf9e8c00efc56295c82b8defed415
parthasarathy <parthasarathy@testpress.in>
2025-12-18 11:47:18 +0530

    data should sent through views

--- forum/templatetags/vote_tags.py
+++ forum/templatetags/vote_tags.py
@@ -1,5 +1,4 @@
 from django import template
-import json
 
 register = template.Library()
 
@@ -31,21 +30,3 @@ def query_string_exclude_page(request):
         del query_params['page']
     
     return query_params.urlencode()
-
-
-@register.filter
-def tags_to_json(queryset):
-    """Convert a tag queryset to JSON array format."""
-    if not queryset:
-        return '[]'
-    tags_data = [{'id': tag.id, 'name': tag.name} for tag in queryset]
-    return json.dumps(tags_data)
-
-
-@register.simple_tag
-def get_selected_tag_ids(request):
-    """Get selected tag IDs from request.GET as JSON array."""
-    if not request or not hasattr(request, 'GET'):
-        return '[]'
-    selected_tag_ids = [int(tag_id) for tag_id in request.GET.getlist('tag') if tag_id.isdigit()]
-    return json.dumps(selected_tag_ids)
